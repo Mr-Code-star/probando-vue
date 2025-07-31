d027bfa35aacea39bacae03cfce784a48f1300b4
Mr-Code-start <vitalybaca92@gmail.com>
2025-07-31 14:05:11 -0500

    feat(user): extend User entity with additional fields for lastname, birth date, gender, and pronoun

--- src/IAM/model/user.entity.js
+++ src/IAM/model/user.entity.js
@@ -1,13 +1,21 @@
 export class User {
     constructor({
-        id = 0,
-        name = '',
-        contact_info = '',
-        password = '',
-                }={}) {
+                    id = 0,
+                    name = '',
+                    lastname = '',
+                    contact_info = '',
+                    password = '',
+                    birth_date = null,
+                    gender = '',
+                    pronoun = null
+                } = {}) {
         this.id = id;
         this.name = name;
+        this.lastname = lastname;
         this.contact_info = contact_info;
         this.password = password;
+        this.birth_date = birth_date;
+        this.gender = gender;
+        this.pronoun = pronoun;
     }
 }
