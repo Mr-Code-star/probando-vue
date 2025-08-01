dd8f22ce369d7ff480351a6cab70bd3ff2367531
Mr-Code-start <vitalybaca92@gmail.com>
2025-08-01 01:31:29 -0500

    feat(i18n): add language switcher component to App.vue for improved localization

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
 <script>
 
+import {defineComponent} from "vue";
+import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+
+export default defineComponent({
+  components: {LanguageSwitcher}
+})
 </script>
 
 <template>
-<main>
-  <router-view></router-view>
-</main>
+  <div class="app-container">
+    <div class="app-header">
+      <language-switcher class="language-switcher-container"></language-switcher>
+    </div>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <style>
+.app-container {
+  position: relative;
+}
 
+.app-header {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
 </style>
