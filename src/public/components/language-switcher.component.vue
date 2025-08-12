00dc247e1d244b2fe781af744c35e09a7b450303
Mr-Code-start <vitalybaca92@gmail.com>
2025-08-11 19:35:31 -0500

    chore(language-switcher): styled container for right alignment

--- src/public/components/language-switcher.component.vue
+++ src/public/components/language-switcher.component.vue
@@ -13,13 +13,20 @@ export default {
 </script>
 
 <template>
-  <pv-select-button v-model="$i18n.locale" :options="languages">
-    <template #option="slotProps">
-      {{ slotProps.option.toUpperCase() }}
-    </template>
-  </pv-select-button>
+  <div class="language-switcher-container">
+    <pv-select-button v-model="$i18n.locale" :options="languages">
+      <template #option="slotProps">
+        {{ slotProps.option.toUpperCase() }}
+      </template>
+    </pv-select-button>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.language-switcher-container {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+}
 
 </style>
